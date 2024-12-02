6d0b51e71646f60443814a2cfb02b8b8348c2d3d
akashb <akash.barde@fyndna.com>
2024-12-02 15:54:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [superAdmins, setSuperAdmins] = useState([]);
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/super-admins')
+            .then((response) => response.json())
+            .then((data) => setSuperAdmins(data));
+
+        fetch('/api/customers')
+            .then((response) => response.json())
+            .then((data) => setCustomers(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Super Admins</h1>
+            <ul>
+                {superAdmins.map((admin) => (
+                    <li key={admin.id}>{admin.name}</li>
+                ))}
+            </ul>
+
+            <h1>Customers</h1>
+            <ul>
+                {customers.map((customer) => (
+                    <li key={customer.id}>{customer.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
